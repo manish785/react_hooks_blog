0d89699afe89a41dd5c7175bdf420e2fffdaa538
manish785 <kumarachilish1997@gmail.com>
2023-08-14 20:55:35 +0530

    Created CreatePost, Home, Navbar, PostDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,16 @@
+import { Route, Routes } from 'react-router-dom';
+import { Navbar, Home, CreatePost, PostDetail } from './';
+
 function App() {
   return (
-    <div className="App">
-      Starting React Blog
+    <div className="container">
+        <Navbar />
+        <Routes>
+           {/* In the latest version of React(i.e - 6), we use element, not the component */}
+          <Route exact path="/" element={ <Home/>} /> 
+          <Route exact path="/post/:postId" element={<PostDetail/>} />
+          <Route exact path="/create-post" element={<CreatePost/>} />
+        </Routes> 
     </div>
   );
 }
