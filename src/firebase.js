ffb07c749c04054b13f3d5f044bdbd5b7f7b6788
manish785 <kumarachilish1997@gmail.com>
2023-08-15 11:28:05 +0530

    Implemented createPost functionality and added data to the firestore db

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,7 @@
 // Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import 'firebase/firestore';
-// import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -18,7 +16,6 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
+firebase.initializeApp(firebaseConfig);
 
 export const firestore = firebase.firestore();
